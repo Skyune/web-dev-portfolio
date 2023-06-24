f6fea8ffb9cd643a761a53403fcb3864d32c2c5c
Krystian <krystianh595@gmail.com>
2023-06-25 00:34:46 +0200

    mobile fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,22 +2,40 @@ import React from 'react';
 import Tilt from 'react-parallax-tilt';
 import './Card.scss';
 
-const Card = ({ image }) => (
-  <Tilt
-    className="parallax-effect-img"
-    tiltMaxAngleX={5}
-    tiltMaxAngleY={5}
-    perspective={800}
-    transitionSpeed={1500}
-    scale={1}
-    gyroscope={true}
-  >
-    <img src={image} alt="" />
-    <div
-      className="inner-element"
-    
-    >Hey</div>
-  </Tilt>
-);
+const Card = ({ image, image2,  index }) => {
+  const isMobile = window.matchMedia('(max-width: 768px)').matches;
+  const cardStyles = {
+  
+  };
+
+  if (isMobile) {
+    return (
+      <div className="parallax-effect-img ">
+        <img src={image} alt="" />
+        <div className="inner-element">
+          <img src={image2} alt="" />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <Tilt
+      className="parallax-effect-img " 
+      style={cardStyles}
+      tiltMaxAngleX={5}
+      tiltMaxAngleY={5}
+      perspective={800}
+      transitionSpeed={1500}
+      scale={1}
+      gyroscope={true}
+    >
+      <img src={image} alt="" />
+      <div className="inner-element">
+        <img src={image2} alt="" />
+      </div>
+    </Tilt>
+  );
+};
 
 export default Card;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,48 +4,62 @@ import './Projects.css'
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-import musicMain from "../assets/img/MainMusic.png";
-import musicSecond from "../assets/img/MusicSecond.png";
+import musicMain from "../assets/img/MainMusic2.png";
+import background from "../assets/img/backgroundMusic.png"
+import musicSecond from "../assets/img/secondMusic2.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import googleBadge from "../assets/img/googleplay.png";
+import queen from "../assets/img/queen.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import  Card  from './Card';
 
 export const Projects = () => {
 
-  
+  const isMobile = window.matchMedia('(max-width: 768px)').matches;
+
 
   return (
     <section className="project" id="project">
 
       
       <Container>
-        <Row>
+        <Row size={12} >
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "": ""}>
                 <h2>Projects</h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                <div class="android-app">
-  <Card image={musicMain} className="img"/>
-  <Card image={musicSecond} className="img"/>
+                <div class="android-app d-flex justify-content-center align-items-center ">
+                <div className="d-md-block mx-auto mobileCard">
 
+  <Card image={background} image2={musicMain} index={0} />
+  </div>
+  <div className="d-none d-xl-block ps-5 pt-5 mobileCard">
 
-  <div>
+  <Card className="text" image={background} image2={musicSecond} index={1}  />
+  </div>
+  <div className="d-none d-md-block ps-5 mt-md-5">
   <div className="lofitext">
-  <h2>Lofi Corner</h2>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-</div>
-  <div class="lofiButtons">
-  <img src={googleBadge} alt="" />
-  <button type="submit">Try demo</button>
-  <span>or see <a href = "">github code</a></span>
-</div>
-</div>
+    <h2>Lofi Corner</h2>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+  </div>
+  <div className="lofiButtons">
+    <img src={googleBadge} alt="" />
+    <button type="submit">Try demo</button>
+    <span>or see <a href="">github code</a></span>
+  </div>
+
+
 </div>
 
+  </div>
+{/*code duplication for mobile. Cuz im lazy. <3*/}
+<div className="lofiButtons d-flex d-md-none pt-3">
+    <img src={googleBadge} alt="" />
+    <span>or see <a href="">github code</a></span>
+  </div>
 
 
               </div>}     
