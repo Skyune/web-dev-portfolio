9b883a1d8ac1504edc34ee3120c6072a058ea054
Krystian <krystianh595@gmail.com>
2023-07-06 02:42:52 +0200

    cardContact

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { ParallaxIlustration } from "./components/ParallaxIlustration.tsx";
+import CardContact  from "./components/CardContact";
 
 function App() {
 
   
   return (
     <div className="App">
+      <CardContact />
 
-      <NavBar3 />
-      <ParallaxIlustration/>
+     {/*  <NavBar3 />
+     <ParallaxIlustration/>*/}
       <Skills/>
       <Projects/>
       <Contact />
