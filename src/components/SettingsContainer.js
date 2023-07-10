e775a97f3a9f2d6b4d5fff5ce25d77b60e5c9268
Skyune <krystianh595@gmail.com>
2023-07-11 01:25:58 +0200

    ui fix

--- src/components/SettingsContainer.js
+++ src/components/SettingsContainer.js
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
-import './cardContact.scss';
-import TrackVisibility from 'react-on-screen';
+import "./cardContact.scss";
+import TrackVisibility from "react-on-screen";
 import { AnimatedSvg } from "./AnimatedSvg";
 import VerticalNav from "./VerticalNav";
 import SettingsIcon from "./IconCutIn";
 import ProfileImg from "../assets/img/progile.webp";
 
-const SettingsContainer = () => {
+const SettingsContainer = ({ hidden }) => {
   const [selectedTheme, setSelectedTheme] = useState("");
-
   useEffect(() => {
     // Load the selected theme from localStorage on component mount
     const savedTheme = localStorage.getItem("selectedTheme");
@@ -29,42 +28,55 @@ const SettingsContainer = () => {
   };
 
   return (
-    <div className="Settings_settingsContainer" style={{ opacity: 1 }}>
-         {/*
+    <div className={`Settings_settingsContainer ${hidden ? "hidden" : ""}`}>
+      {/*
       <button className="ThemeSelect_buttonTheme button--theme">
         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
           <path d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" />
         </svg>
       </button>
-      */
-}
-      <div className="ColorSelect_colorSelect" style={{ opacity: 1 }}>
+      */}
+      <div className={`ColorSelect_colorSelect  ${hidden ? "hidden" : ""}`}>
         <button
-          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_orange ${selectedTheme === "orange" ? "ColorSelect_colorButtonActive" : ""}`}
+          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_orange ${
+            selectedTheme === "orange" ? "ColorSelect_colorButtonActive" : ""
+          }`}
           onClick={() => handleThemeChange("orange")}
         />
         <button
-          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_yellow ${selectedTheme === "yellow" ? "ColorSelect_colorButtonActive" : ""}`}
+          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_yellow ${
+            selectedTheme === "yellow" ? "ColorSelect_colorButtonActive" : ""
+          }`}
           onClick={() => handleThemeChange("yellow")}
         />
         <button
-          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_blue ${selectedTheme === "blue" ? "ColorSelect_colorButtonActive" : ""}`}
+          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_blue ${
+            selectedTheme === "blue" ? "ColorSelect_colorButtonActive" : ""
+          }`}
           onClick={() => handleThemeChange("blue")}
         />
         <button
-          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_red ${selectedTheme === "red" ? "ColorSelect_colorButtonActive" : ""}`}
+          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_red ${
+            selectedTheme === "red" ? "ColorSelect_colorButtonActive" : ""
+          }`}
           onClick={() => handleThemeChange("red")}
         />
         <button
-          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_purple ${selectedTheme === "purple" ? "ColorSelect_colorButtonActive" : ""}`}
+          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_purple ${
+            selectedTheme === "purple" ? "ColorSelect_colorButtonActive" : ""
+          }`}
           onClick={() => handleThemeChange("purple")}
         />
         <button
-          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_green ${selectedTheme === "green" ? "ColorSelect_colorButtonActive" : ""}`}
+          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_green ${
+            selectedTheme === "green" ? "ColorSelect_colorButtonActive" : ""
+          }`}
           onClick={() => handleThemeChange("green")}
         />
         <button
-          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_pink ${selectedTheme === "pink" ? "ColorSelect_colorButtonActive" : ""}`}
+          className={`ColorSelect_colorButton ColorSelect_colorButtonPallet ColorSelect_pink ${
+            selectedTheme === "pink" ? "ColorSelect_colorButtonActive" : ""
+          }`}
           onClick={() => handleThemeChange("pink")}
         />
       </div>
